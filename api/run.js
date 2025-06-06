fec46121410336f2a9455d67d57ccd7720f7db81
Andrés Lugo <andres.lugo.97@outlook.com>
2025-06-06 11:01:34 -0500

    feat: adjust

--- api/run.js
+++ api/run.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-export default async function handler(req, res) {
-  const { exec } = await import('child_process')
-  exec('node run-lighthouse.js', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`)
-      return res.status(500).send('Failed')
-    }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`)
-    }
-    console.log(`stdout: ${stdout}`)
-    res.status(200).send('Lighthouse run completed')
-  })
-}
-=======
 const path = require('path');
 
 module.exports = async function handler(req, res) {
@@ -35,4 +19,3 @@ module.exports = async function handler(req, res) {
     return res.status(500).json({ error: error.message });
   }
 };
->>>>>>> development
